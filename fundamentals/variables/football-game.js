62fa54db43d09a95339485b62303b459914a44ba
Brandon Cedeño <brandoncede1815@gmail.com>
2025-01-28 12:32:57 -0600

    chore(variables): add football-game excercise

---
+++ fundamentals/variables/football-game.js
@@ -0,0 +1,23 @@
+const eventName = "Eliminatorias Sudamericanas"
+
+console.log(eventName)
+
+const date = "22/11/23"
+const hour = "20:30"
+
+console.log(date)
+console.log(hour)
+
+const localTeam = "Argentina"
+const visitTeam = "Brasil"
+
+const game = localTeam + " vs " + visitTeam
+
+console.log(game)
+
+const localTeamScore = 1
+const visitTeamScore = 0
+
+const score = localTeamScore + "-" + visitTeamScore
+
+console.log(score)
