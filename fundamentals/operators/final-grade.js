f96dfef10e3c13c5dd89dfcbade7c076c7bb0a0c
Brandon Cedeño <brandoncede1815@gmail.com>
2025-01-29 10:11:14 -0600

    chore(operators): add final-grade exercise

---
+++ fundamentals/operators/final-grade.js
@@ -0,0 +1,11 @@
+const prompt = require("prompt-sync")()
+
+const TOTAL_GRADE = 3
+const MAX_DIGITS = 2
+const grade1 = parseInt(prompt("Nota #1 "))
+const grade2 = parseInt(prompt("Nota #2 "))
+const grade3 = parseInt(prompt("Nota #3 "))
+
+const average = (grade1 + grade2 + grade3) / TOTAL_GRADE
+
+console.log(`Final Grade: ${average.toFixed(MAX_DIGITS)}`)
