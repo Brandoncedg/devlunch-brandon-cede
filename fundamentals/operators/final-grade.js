65ac0a6ba52c8f6ba4b97419b6b0870a00e5a347
Brandon Cedeño <brandoncede1815@gmail.com>
2025-01-29 10:29:33 -0600

    chore(operators): add days-to-month exercise

--- fundamentals/operators/final-grade.js
+++ fundamentals/operators/final-grade.js
@@ -2,9 +2,9 @@ const prompt = require("prompt-sync")()
 
 const TOTAL_GRADE = 3
 const MAX_DIGITS = 2
-const grade1 = parseInt(prompt("Nota #1 "))
-const grade2 = parseInt(prompt("Nota #2 "))
-const grade3 = parseInt(prompt("Nota #3 "))
+const grade1 = parseFloat(prompt("Nota #1 "))
+const grade2 = parseFloat(prompt("Nota #2 "))
+const grade3 = parseFloat(prompt("Nota #3 "))
 
 const average = (grade1 + grade2 + grade3) / TOTAL_GRADE
 
