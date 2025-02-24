83779bbadaeed23619da90659b3ac4095b75eff3
Brandon Cedeño <brandoncede1815@gmail.com>
2025-02-24 15:35:03 -0600

    chore(objects): add fraction exercise

---
+++ object-oriented-programming/objects/fraction.js
@@ -0,0 +1,67 @@
+const createFraction = (numerator, denominator) => {
+    return {
+      numerator,
+      denominator
+    };
+  };
+  
+  // 🔹 ADDITION OF FRACTIONS
+  const addFractions = (f1, f2) => {
+    const numerator = (f1.numerator * f2.denominator) + (f1.denominator * f2.numerator);
+    const denominator = f1.denominator * f2.denominator;
+  
+    return createFraction(numerator, denominator);
+  };
+  
+  // 🔹 SUBTRACTION OF FRACTIONS
+  const subtractFractions = (f1, f2) => {
+    const numerator = (f1.numerator * f2.denominator) - (f1.denominator * f2.numerator);
+    const denominator = f1.denominator * f2.denominator;
+  
+    return createFraction(numerator, denominator);
+  };
+  
+  // 🔹 MULTIPLICATION OF FRACTIONS
+  const multiplyFractions = (f1, f2) => {
+    const numerator = f1.numerator * f2.numerator;
+    const denominator = f1.denominator * f2.denominator;
+  
+    return createFraction(numerator, denominator);
+  };
+  
+  // 🔹 DIVISION OF FRACTIONS
+  const divideFractions = (f1, f2) => {
+    const numerator = f1.numerator * f2.denominator;
+    const denominator = f1.denominator * f2.numerator;
+  
+    return createFraction(numerator, denominator);
+  };
+
+  const createFractionWithFunctions = (numerator, denominator) => {
+    const f1 = createFraction(numerator, denominator)
+  
+    return {
+      ...f1,
+      addFractions: (f2) => addFractions(f1, f2),
+      subtractFractions: (f2) => subtractFractions(f1, f2),
+      multiplyFractions: (f2) => multiplyFractions(f1, f2),
+      divideFractions: (f2) => divideFractions(f1, f2),
+    }
+  }
+  
+  // 🔹 TESTS
+  const f1 = createFraction(3, 4);
+  const f2 = createFraction(2, 3);
+  
+  console.log("Addition:", addFractions(f1, f2));
+  console.log("Subtraction:", subtractFractions(f1, f2));
+  console.log("Multiplication:", multiplyFractions(f1, f2));
+  console.log("Division:", divideFractions(f1, f2));
+
+  const f3 = createFractionWithFunctions(3,4)
+  const f4 = createFraction(2,3)
+
+  console.log(f3.addFractions(f4))
+  console.log(f3.subtractFractions(f4))
+  console.log(f3.multiplyFractions(f4))
+  console.log(f3.divideFractions(f4))
